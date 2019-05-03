58acef8fe0cca24d3ae531d278dd8ba34dd59490
Thomas Varjavandi <thomas@varjavandi.com>
2019-05-03 16:54:32 +0200

    Modified 3 files modtly formmating

--- src/jpu2016/dogfight/modele/Cloud.java
+++ src/jpu2016/dogfight/modele/Cloud.java
@@ -1,2 +1,14 @@
 package jpu2016.dogfight.modele;
 
+public class Cloud extends Mobile{
+
+    private int SPEED = 4;
+    private int WIDTH = 100;
+    private int HEIGHT = 10;
+    private String IMAGE = "cloud";
+
+    public Cloud(Direction direction, Dimension dimension) {
+        super(direction, position, null, player, image);
+    }
+
+}
--- src/jpu2016/dogfight/modele/Missle.java
+++ src/jpu2016/dogfight/modele/Missle.java
@@ -1,1 +1,32 @@
 package jpu2016.dogfight.modele;
+
+public class Missle extends Mobile{
+
+    private int SPEED = 4;
+    private int WIDTH = 100;
+    private int HEIGHT = 10;
+    private int MAX_DISTANCE_TRAVELED = 1400;
+    private String IMAGE = "missile";
+    private int distanceTraveled = 0;
+    
+    public Missle(Direction direction, Dimension dimension) {
+        super(direction, position, null, player, image);
+    }
+
+    public int getWidthWithADirection(Direction direction) {
+
+    }
+
+    public int getHeightWithADirection(Direction direction) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public boolean isWeapon() {
+
+    }
+
+}
