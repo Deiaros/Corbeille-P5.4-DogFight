60d0f58a992ba8d1aa62038cc83c71a71b14e381
Dedelweiss <44496264+Dedelweiss@users.noreply.github.com>
2019-05-03 13:41:24 +0200

    Create UserOrder.java

---
+++ src/jpu2016/dogfight/controleur/UserOrder.java
@@ -0,0 +1,26 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder implements IUserOrder {
+
+	private int player;
+	private Order order;
+	
+	public UserOrder(int player, Order order)
+	{
+		this.setPlayer(player);
+		this.order = order;
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+	}
+
+	public Order getOrder()
+	{
+		return order;
+	}
+}
