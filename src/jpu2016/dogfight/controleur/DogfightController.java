2627032733bab3b6740f940a0405da5ac0cc538c
Dedelweiss <44496264+Dedelweiss@users.noreply.github.com>
2019-05-03 13:38:05 +0200

    Update DogfightController.java

--- src/jpu2016/dogfight/controleur/DogfightController.java
+++ src/jpu2016/dogfight/controleur/DogfightController.java
@@ -1,1 +1,35 @@
 package jpu2016.dogfight.controleur;
+
+public class DogfightController {
+private int TIME_SLEEP;
+
+public DogfightController()
+{
+	TIME_SLEEP = 30;
+}
+public DogfightController (IDogfightModel dogfightModel)
+{
+	
+}
+
+public void orderPerform(UserOrder userOrder)
+{
+	
+}
+public void play()
+{
+	
+}
+public void setViewSystem(IViewSystem viewSystem)
+{
+	
+}
+private void lauchMissile (int player)
+{
+	
+}
+private void gameLoop()
+{
+	
+}
+}
