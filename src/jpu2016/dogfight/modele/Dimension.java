c7ce3dc264f49e81e3deff74e0e6b6a6e1b5e9fc
Thomas Varjavandi <thomas@varjavandi.com>
2019-05-03 14:20:56 +0200

    Modified Dimension and Position

--- src/jpu2016/dogfight/modele/Dimension.java
+++ src/jpu2016/dogfight/modele/Dimension.java
@@ -2,5 +2,30 @@ package jpu2016.dogfight.modele;
 
 public class Dimension {
 
-    private int 
+    private int width;
+    private int height;
+
+    public Dimension(int width, int height) {
+        
+    }
+
+    public Dimension(Dimension dimension) {
+
+    }
+
+    public int getWidth() {
+        return 0; //temp REMOVE ME
+    }
+
+    public void setWidth(int width) {
+
+    }
+
+    public int getHeight() {
+        return 0; //temp REMOVE ME
+    }
+
+    public void setHeight(int height) {
+
+    }
 }
--- src/jpu2016/dogfight/modele/Position.java
+++ src/jpu2016/dogfight/modele/Position.java
@@ -1,1 +1,19 @@
 package jpu2016.dogfight.modele;
+
+public class Position {
+
+    private double x;
+    private double y;
+    private double maxX;
+    private double maxY;
+
+    public Position (double x, double y, double maxX, double maxY) {
+
+    }
+
+    public Position (Position position) {
+
+    }
+
+    
+}
