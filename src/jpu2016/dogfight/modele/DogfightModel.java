d7be2d67609c38454ad45ec4623a270ee7116df2
Thomas Varjavandi <thomas@varjavandi.com>
2019-05-06 09:26:58 +0200

    DOgfight

--- src/jpu2016/dogfight/modele/DogfightModel.java
+++ src/jpu2016/dogfight/modele/DogfightModel.java
@@ -1,27 +1,35 @@
 package jpu2016.dogfight.modele;
 
+import java.awt.List;
 import java.util.ArrayList;
 
 public class DogfightModel implements IDogfightModel {
+
+    public Sky levelone;
+    public List<Mobile> Plane = new ArrayList<Plane>();
 
     public DogfightModel() {
 
     }
 
     public IArea getArea() {
-        return null;//temp REMOVE ME
+        return levelone.getDimension();//temp REMOVE ME
     }
 
     public void buildArea(Dimension dimension) {
+        this.levelone = new Sky(dimension);
         //called by the controller to help build the game with Sky
     }
 
     public void addMobile(IMobile mobile) {
-        
+        Mobile plane1 = new Plane(1, UP, new Position(0, 0, 100, 100), "test1");
+        Mobile plane2 = new Plane(1, UP, new Position(0, 0, 100, 100), "test2");
+       // Mobile test = new Mobile Plane(Imobile.get, direction, position, image)
     }
 
     public void removeMobile(IMobile mobile) {
-
+        plane1 = null;
+        plane2 = null;
     }
 
     
